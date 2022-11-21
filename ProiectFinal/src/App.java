99f01f2456ad3c32a5b776f98468a272346d64a6
Edmond Volosciuc <volosciucedmond@gmail.com>
2022-11-21 16:58:16 +0200

    versiunea1

--- ProiectFinal/src/App.java
+++ ProiectFinal/src/App.java
@@ -8,38 +8,39 @@ class KbcTest implements ActionListener {
     JButton b1, b2;
     JLabel lb1, lb2;
     ButtonGroup bg;
-    String ques[] = {};
-    String op1[] = { "Tim", "peris" };
-    String op2[] = { "steve", "Delhi" };
-    String op3[] = { "mark", "Rome" };
-    String op4[] = { "james", "bern" };
-    String ans[] = { "james", "Rome" };
+    String ques[] = {"Soarele este o planetă, sau o stea?", "Cine a scris cartea Ciuleandra?", "De câti jucători este nevoie într-p echipăde rugbi?",
+    "Câti ani a durat Războiul de 100 de ani?"};
+    String op1[] = { "Stea", "Liviu Rebreanu", "12", "100"};
+    String op2[] = { "Planetă", "Ion Creangă", "13", "101"};
+    String op3[] = { "", "Mihai Eminescu", "14", "103"};
+    String op4[] = { "", "George Bacovia", "15" , "116"};
+    String ans[] = { "Stea", "Liviu Rebreanu", "15", "116"};
     int cn;
 
     KbcTest() {
         fr = new JFrame();
         fr.setLayout(null);
-        fr.setSize(600, 600);
+        fr.setSize(800, 800);
         Container c = fr.getContentPane();
         c.setBackground(Color.lightGray);
 
         lb1 = new JLabel(ques[0]);
-        lb1.setBounds(50, 50, 300, 30);
+        lb1.setBounds(200, 50, 800, 30);
         fr.add(lb1);
         lb1.setFont(new Font("chiller", Font.BOLD, 30));
 
         rb1 = new JRadioButton(op1[0]);
-        rb1.setBounds(100, 120, 100, 30);
+        rb1.setBounds(200, 120, 100, 30);
         fr.add(rb1);
 
         rb2 = new JRadioButton(op2[0]);
-        rb2.setBounds(350, 120, 100, 30);
+        rb2.setBounds(400, 120, 100, 30);
         fr.add(rb2);
         rb3 = new JRadioButton(op3[0]);
-        rb3.setBounds(100, 200, 100, 30);
+        rb3.setBounds(200, 200, 100, 30);
         fr.add(rb3);
         rb4 = new JRadioButton(op4[0]);
-        rb4.setBounds(350, 200, 100, 30);
+        rb4.setBounds(400, 200, 100, 30);
         fr.add(rb4);
         bg = new ButtonGroup();
         bg.add(rb1);
@@ -51,12 +52,12 @@ class KbcTest implements ActionListener {
         rb3.addActionListener(this);
         rb4.addActionListener(this);
 
-        b1 = new JButton("Sumbit");
-        b1.setBounds(100, 400, 100, 30);
+        b1 = new JButton("Verifică răspunsul");
+        b1.setBounds(200, 400, 150, 30);
         fr.add(b1);
 
-        b2 = new JButton("Next");
-        b2.setBounds(250, 400, 100, 30);
+        b2 = new JButton("Mai departe");
+        b2.setBounds(350, 400, 150, 30);
         fr.add(b2);
         b1.addActionListener(this);
         b2.addActionListener(this);
@@ -81,9 +82,9 @@ class KbcTest implements ActionListener {
             if (rb4.isSelected())
                 en = rb4.getText();
             if (en.equals(ans[cn]))
-                JOptionPane.showMessageDialog(null, "Right Answer");
+                JOptionPane.showMessageDialog(null, "Răspuns corect");
             else
-                JOptionPane.showMessageDialog(null, "Wrong Answer");
+                JOptionPane.showMessageDialog(null, "Răspuns greșit");
         }
         if (e.getSource() == b2) {
             cn++;
